534bbb75ade1b156edaca89c1609cee5925b4831
JosephRechdiner <jrechdiner2002@gmail.com>
2026-01-29 17:21:14 +0200

    added routes

--- app/routes/employees.py
+++ app/routes/employees.py
@@ -1,22 +1,29 @@
 from typing import List
 from fastapi import APIRouter
-from app.models import EmployeeBase, EmployeeCreate, EmployeeResponse
+from app.models import EmployeeBase, EmployeeCreate, EmployeeResponse, EmployeeUpdate
 
 
 employees_routes = APIRouter()
 
-@employees_routes.get("/employees")
+@employees_routes.get("/employees", response_model=List[EmployeeResponse])
 def employees():
     return db.get_all_employees()
 
-@employees_routes.get("/employees/{emp_id}", response_model=EmployeeCreate)
+@employees_routes.get("/employees/{emp_id}", response_model=EmployeeResponse)
 def employee_by_id(emp_id: str):
     return db.get_employee_by_id(emp_id)
 
 @employees_routes.post("/employees", response_model=EmployeeCreate)
-def create_employees(employees: EmployeeBase):
+def create_employee(employees: EmployeeBase):
     return db.add_employee(employees)
 
-@employees_routes.get("/employees")
-def get_employees():
-    return db.get_all_employees()
+@employees_routes.put("/employees/{emp_id}")
+def put_employee(emp_id: str, employees: EmployeeUpdate):
+    return db.update_employee(emp_id, employees)
+
+@employees_routes.delete("/employees/{emp_id}")
+def put_employees(emp_id: str):
+    has_deleted = db.delete_employee(emp_id)
+    if has_deleted:
+        return {"msg": "Deleted"}
+    return {"msg": "Not deleted"}
